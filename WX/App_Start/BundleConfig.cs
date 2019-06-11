5e7487636162305c1d332610e76314287ee44f2a
Administrator <Administrator@DESKTOP-BR09NBL>
2019-06-11 17:42:57 +0800

    test

--- WX/App_Start/BundleConfig.cs
+++ WX/App_Start/BundleConfig.cs
@@ -22,7 +22,7 @@ namespace WX
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
+            bundles.Add(new StyleBundle("~/Content/css").Include( 
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
         }
